f592615aa1067cb809dd8427123e698e158c87a2
shogo7 <76215044+shogo7@users.noreply.github.com>
2025-04-28 00:53:42 +0900

    add shared

--- server/src/controllers/fruit.ts
+++ server/src/controllers/fruit.ts
@@ -1,15 +1,18 @@
 // controllers/fruit.ts
 import { Fruit } from "../models/fruit.js";
 import type { FruitType } from "../models/fruit.js";
+import { formatDate } from "@shared/dateUtils.js";
+
+console.log(formatDate(new Date()));
 
 export async function getAllFruits(): Promise<FruitType[]> {
-    const fruits = await Fruit.find().lean();
-    return fruits.map((fruit) => ({
-      ...fruit,
-      _id: fruit._id.toString(),
-    }));
-  }
-  
+  const fruits = await Fruit.find().lean();
+  return fruits.map((fruit) => ({
+    ...fruit,
+    _id: fruit._id.toString(),
+  }));
+}
+
 export async function getFruitById(id: string): Promise<FruitType> {
   const fruit = await Fruit.findById(id).lean();
   if (!fruit) throw new Error("Not found");
---
+++ shared/dateUtils.ts
@@ -0,0 +1,11 @@
+import dayjs from "dayjs";
+
+export function formatDate(date: Date | string, format: string = "YYYY-MM-DD") {
+  // ハート付き日付フォーマット関数
+  const formattedDate = dayjs(date).format(format);
+  
+  // 数字をハート付きに変換
+  return formattedDate.replace(/[0-9]/g, digit => {
+    return digit + "❤️";
+  });
+}
